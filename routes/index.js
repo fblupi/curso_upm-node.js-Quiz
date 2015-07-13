1bfa77be6b1fb3b2e17403afeee9bd7b023a41a2
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-07-13 09:15:28 +0200

    Crear comentario

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -11,14 +12,16 @@ router.get('/', function(req, res) {
 // Autoload de comandos :quizId
 router.param('quizId', quizController.load); // autoload :quizId
 
-router.get('/author',                       quizController.author);
-router.get('/quizes',                       quizController.index);
-router.get('/quizes/:quizId(\\d+)',         quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-router.get('/quizes/new',                   quizController.new);
-router.post('/quizes/create',               quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
-router.put('/quizes/:quizId(\\d+)',         quizController.update);
-router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
+router.get('/author',                           quizController.author);
+router.get('/quizes',                           quizController.index);
+router.get('/quizes/:quizId(\\d+)',             quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',      quizController.answer);
+router.get('/quizes/new',                       quizController.new);
+router.post('/quizes/create',                   quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit);
+router.put('/quizes/:quizId(\\d+)',             quizController.update);
+router.delete('/quizes/:quizId(\\d+)',          quizController.destroy);
+router.get('/quizes/:quizId(\\d+)/comments/new',commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',   commentController.create);
 
 module.exports = router;
